e40daecaa57c9c659c269faa0b924433362db4c3
danije.filipovic <danijelf89@hotmail.com>
2023-02-15 19:28:42 +0100

    month chooser component deleted

--- src/app/body-info/body-info.module.ts
+++ src/app/body-info/body-info.module.ts
@@ -1,12 +1,9 @@
-import { Component, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { BodyInfoComponent } from './body-info.component';
 import { RouterModule, Routes } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
 import { SharedModuleModule } from '../shared/shared-module/shared-module.module';
 import { UserMenuModule } from '../user-menu/user-menu.module';
 import { UserInfoModule } from '../user-info/user-info.module';
-import { MonthChooserModule } from '../month-chooser/month-chooser.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BodyInfoService } from './body-info.service';
 
@@ -20,7 +17,6 @@ const routes: Routes = [
 @NgModule({
   declarations: [BodyInfoComponent],
   imports: [
-    MonthChooserModule,
     RouterModule.forChild(routes),
     SharedModuleModule,
     UserMenuModule,
