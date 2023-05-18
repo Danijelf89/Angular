6276927d4687c1d6b75dbaff120e3e609fd25979
danijel.filipovic <danijel.filipovic@comdata.rs>
2023-05-18 12:16:35 +0200

    filter by month and date implemented for body data page, bug for filtering by date fixes on bidy chart page, button for adding new record on body page placed in center

--- src/app/body-info/body-info.component.ts
+++ src/app/body-info/body-info.component.ts
@@ -1,67 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { BodyInfoService } from './body-info.service';
 
 @Component({
   selector: 'app-body-info',
   templateUrl: './body-info.component.html',
-  styleUrls: ['./body-info.component.css']
+  styleUrls: ['./body-info.component.css'],
 })
-export class BodyInfoComponent {
-
+export class BodyInfoComponent implements OnInit {
   bodyInfoData$ = this.service.bodyInfoData$;
   nothingToShow$ = this.service.nothingToShow$;
   chosenMonths$ = this.service.chosenDate$;
   chosenYear$ = this.service.chosenYear$;
 
-  constructor(private service : BodyInfoService)
-  {
-    this.service.getAllData();
+  constructor(private service: BodyInfoService) {}
+  ngOnInit(): void {
+    this.service.setInitalFilterData();
   }
 
-
   monthsForm = new FormGroup({
-    
-    januaryIsChecked : new FormControl(false),
-    februarysChecked : new FormControl(false),
-    marchsChecked : new FormControl(false),
-    aprilsChecked : new FormControl(false),
-    maysChecked : new FormControl(false),
-    junsChecked : new FormControl(false),
-    julysChecked : new FormControl(false),
-    avgustsChecked : new FormControl(false),
-    septembersChecked : new FormControl(false),
-    octobersChecked : new FormControl(false),
-    novembersChecked : new FormControl(false),
-    decembersChecked : new FormControl(false),
-    chosenYear : new FormControl('')
-
-    
-  })
+    januaryIsChecked: new FormControl(false),
+    februarysChecked: new FormControl(false),
+    marchsChecked: new FormControl(false),
+    aprilsChecked: new FormControl(false),
+    maysChecked: new FormControl(false),
+    junsChecked: new FormControl(false),
+    julysChecked: new FormControl(false),
+    avgustsChecked: new FormControl(false),
+    septembersChecked: new FormControl(false),
+    octobersChecked: new FormControl(false),
+    novembersChecked: new FormControl(false),
+    decembersChecked: new FormControl(false),
+    chosenYear: new FormControl(''),
+  });
 
   handleChangeYearChanged(e: any) {
-    
-
     this.monthsForm.patchValue({
       chosenYear: e.detail.value,
     });
-    
+
+    this.service.yearChanged(e.detail.value);
   }
 
-  
-  back()
-  {
+  back() {
     this.service.navigateBack();
   }
 
-  menuClosed()
-  {
+  menuClosed() {
     this.service.loadMonths(this.monthsForm);
   }
 
-  openChartDetails()
-  {
+  openChartDetails() {
     this.service.openChartDetails();
   }
-
 }
--- src/app/body-info/body-info.service.ts
+++ src/app/body-info/body-info.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 import { BodyInfo } from '../models/body-info/body-info';
 
 @Injectable({
@@ -35,12 +35,40 @@ export class BodyInfoService {
     this.chosenYear$ = this.chosenYearSubject.asObservable();
   }
 
-  getAllData() {
+  setInitalFilterData()
+  {
     let td: Date = new Date();
     this.chosenYearSubject.next(td.getFullYear().toString());
     this.chosenDateSubject.next(this.createMonthsList(td.getMonth()));
 
-    return this.httpClient.get<BodyInfo[]>(this.mockDataUrl).subscribe({
+    this.getAllData([td.getMonth()]);
+  }
+
+  getAllData(months : number[]) {
+    
+
+    return this.httpClient.get<BodyInfo[]>(this.mockDataUrl).pipe(
+      map((bodyinfo : BodyInfo[]) => {
+        let bodyList : BodyInfo[] = [];
+
+        bodyinfo.forEach(element => {
+          console.log('element month', new Date(element.messurementDate).getMonth());
+          console.log('months list', this.months);
+
+          if(new Date(element.messurementDate).getFullYear().toString() == this.chosenYearSubject.value
+          && months.includes(new Date(element.messurementDate).getMonth())
+          )
+          {
+            
+            bodyList.push(element);
+          }
+        });
+
+        
+        return bodyList;
+        
+      }),
+    ).subscribe({
       next: (data) => {
         this.bodyInfoDataSubject.next(data);
         if (data.length > 0) {
@@ -48,16 +76,22 @@ export class BodyInfoService {
         } else {
           this.nothingToShoeSubject.next(true);
         }
-        console.log('data:' + data);
+        console.log('body info data filtered', data);
       },
       error: (err) => {
         console.log('error' + err);
       },
     });
+  
   }
 
   async openChartDetails() {
     this.router.navigate(['body-info/body-info-charts']);
+  }
+
+  yearChanged(year : string){
+    this.chosenYearSubject.next(year);
+    this.getAllData(this.months);
   }
 
   loadMonths(monthsForm: FormGroup) {
@@ -65,55 +99,56 @@ export class BodyInfoService {
     let chosenMonthsSTring: string[] = [];
 
     if (monthsForm.value.januaryIsChecked) {
+      this.months.push(0);
+      chosenMonthsSTring.push(this.createMonthsList(0));
+    }
+    if (monthsForm.value.februarysChecked) {
       this.months.push(1);
       chosenMonthsSTring.push(this.createMonthsList(1));
     }
-    if (monthsForm.value.februarysChecked) {
+
+    if (monthsForm.value.marchsChecked) {
       this.months.push(2);
       chosenMonthsSTring.push(this.createMonthsList(2));
     }
-
-    if (monthsForm.value.marchsChecked) {
-      chosenMonthsSTring.push(this.createMonthsList(3));
-    }
     if (monthsForm.value.aprilsChecked) {
-      this.months.push(4);
-      chosenMonthsSTring.push(this.createMonthsList(4));
+      this.months.push(3);
+      chosenMonthsSTring.push(this.createMonthsList(3));
     }
 
     if (monthsForm.value.maysChecked) {
+      this.months.push(4);
+      chosenMonthsSTring.push(this.createMonthsList(4));
+    }
+    if (monthsForm.value.junsChecked) {
       this.months.push(5);
       chosenMonthsSTring.push(this.createMonthsList(5));
     }
-    if (monthsForm.value.junsChecked) {
+
+    if (monthsForm.value.julysChecked) {
       this.months.push(6);
       chosenMonthsSTring.push(this.createMonthsList(6));
     }
-
-    if (monthsForm.value.julysChecked) {
+    if (monthsForm.value.avgustsChecked) {
       this.months.push(7);
       chosenMonthsSTring.push(this.createMonthsList(7));
     }
-    if (monthsForm.value.avgustsChecked) {
+    if (monthsForm.value.septembersChecked) {
       this.months.push(8);
       chosenMonthsSTring.push(this.createMonthsList(8));
     }
-    if (monthsForm.value.septembersChecked) {
+
+    if (monthsForm.value.octobersChecked) {
       this.months.push(9);
       chosenMonthsSTring.push(this.createMonthsList(9));
     }
-
-    if (monthsForm.value.octobersChecked) {
+    if (monthsForm.value.novembersChecked) {
       this.months.push(10);
       chosenMonthsSTring.push(this.createMonthsList(10));
     }
-    if (monthsForm.value.novembersChecked) {
+    if (monthsForm.value.decembersChecked) {
       this.months.push(11);
       chosenMonthsSTring.push(this.createMonthsList(11));
-    }
-    if (monthsForm.value.decembersChecked) {
-      this.months.push(12);
-      chosenMonthsSTring.push(this.createMonthsList(12));
     }
 
     if (monthsForm.value.chosenYear != '') {
@@ -124,60 +159,62 @@ export class BodyInfoService {
     }
 
     this.chosenDateSubject.next(chosenMonthsSTring.join(', '));
+
+    this.getAllData(this.months);
   }
 
   createMonthsList(monthNumb: number) : string  {
     
 
     switch (monthNumb) {
-      case 1: {
+      case 0: {
         return 'Januar'
         
       }
-      case 2: {
+      case 1: {
         return 'Februar';
        
       }
-      case 3: {
+      case 2: {
         return 'Mart';
         
       }
-      case 4: {
+      case 3: {
         return 'April';
         
       }
-      case 5: {
+      case 4: {
         return 'Maj';
         
       }
-      case 6: {
+      case 5: {
         return 'Jun';
         
       }
 
-      case 7: {
+      case 6: {
         return 'Jul';
        
       }
-      case 8: {
+      case 7: {
         return 'Avgust';
        
       }
 
-      case 9: {
+      case 8: {
         return 'Septembar';
         
       }
-      case 10: {
+      case 9: {
         return 'Oktobar';
      
       }
 
-      case 11: {
+      case 10: {
         return 'Novembar';
        
       }
-      case 12: {
+      case 11: {
         return 'Decembar';
       
       }
